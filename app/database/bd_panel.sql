55ab058e3fa6c6354ea16a2a982afac117124313
ADJUMANY YANN AXEL VIANNEY <adjumany0072902@univmetiers.ci>
2025-05-19 09:33:33 +0000

    database file

--- app/database/bd_panel.sql
+++ app/database/bd_panel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 15, 2025 at 07:59 PM
+-- Generation Time: May 19, 2025 at 09:24 AM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `client` (
 --
 
 INSERT INTO `client` (`id`, `nom`, `prenom`, `adresse`, `contact`) VALUES
-(1, 'Axel', 'Yann', 'Abidjan', '0102030405'),
+(1, 'ADJUMANY', 'Yann Axel', 'Abidjan', '0102030405'),
 (2, 'Zouzou', 'Patrick', 'Yopougon', '0596639774');
 
 -- --------------------------------------------------------
@@ -59,10 +59,17 @@ CREATE TABLE IF NOT EXISTS `paiement` (
   `montant` int NOT NULL,
   `mode` varchar(25) NOT NULL,
   `statut` varchar(25) NOT NULL,
-  `client_id` int NOT NULL,
+  `reservation_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `client_id` (`client_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `reservation_id` (`reservation_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `paiement`
+--
+
+INSERT INTO `paiement` (`id`, `date`, `montant`, `mode`, `statut`, `reservation_id`) VALUES
+(3, '2025-05-17', 4464000, 'Orange Money', 'Soldé', 9);
 
 -- --------------------------------------------------------
 
@@ -83,17 +90,27 @@ CREATE TABLE IF NOT EXISTS `panneau` (
   `reservation_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `reservation_id` (`reservation_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `panneau`
 --
 
 INSERT INTO `panneau` (`id`, `emplacement`, `longueur`, `largeur`, `type`, `etat`, `prix`, `description`, `reservation_id`) VALUES
-(2, 'Abidjan, Cocody', 2, 4, 'Panneaux modernes', 'Réservé', 25000, 'test', 3),
-(3, 'Abidjan, Cocody', 3, 5, 'Panneaux classiques', 'Réservé', 30000, 'Ce panneau moderne, situé dans le quartier prisé de Cocody à Abidjan, offre une visibilité optimale pour vos campagnes publicitaires. Avec ses dimensions de 2 mètres sur 4, il est idéal pour capter l’attention dans un environnement urbain dynamique.', 5),
-(4, 'Abidjan, Yopougon', 3, 5, 'Big size', 'Réservé', 30000, 'test', 4),
-(5, 'Abidjan, Cocody', 10, 11, 'Panneaux modernes', 'Réservé', 15000, 'test', 6);
+(2, 'Abidjan, Cocody', 2, 4, 'Panneaux modernes', 'Réservé', 12000, 'test', 0),
+(3, 'Abidjan, Cocody', 3, 5, 'Panneaux classiques', 'Réservé', 10000, 'Ce panneau moderne, situé dans le quartier prisé de Cocody à Abidjan, offre une visibilité optimale pour vos campagnes publicitaires. Avec ses dimensions de 2 mètres sur 4, il est idéal pour capter l’attention dans un environnement urbain dynamique.', 0),
+(4, 'Abidjan, Yopougon', 3, 5, 'Big size', 'Réservé', 20000, 'test', 0),
+(5, 'Abidjan, Cocody', 3, 4, 'Panneaux modernes', 'Réservé', 15000, 'test', 0),
+(6, 'Abidjan - Plateau, Avenue Chardy', 4, 3, 'Panneaux modernes', 'Réservé', 15000, 'Grand panneau idéal pour campagnes urbaines.', 0),
+(7, 'Yopougon - Sideci, en face de la pharmacie Akadjoba', 3.5, 2.5, 'Panneaux classiques', 'Réservé', 12000, 'Visible depuis une route très fréquentée.', 0),
+(8, 'Cocody - Deux Plateaux, Vallon', 5, 3, 'City led', 'Disponible', 20000, 'Panneau numérique haute visibilité.', 0),
+(9, 'Marcory - Zone 4, rue Paul Langevin', 3, 2, 'Panneaux classiques', 'Disponible', 10000, 'Bonne exposition au carrefour principal.', 0),
+(10, 'Treichville - Boulevard de Marseille', 4.5, 3.5, 'Panneaux modernes', 'Disponible', 13000, 'Panneau situé en bordure de voie rapide.', 0),
+(11, 'Abobo - Rond-point Gagnoa', 3, 2, 'Panneaux classiques', 'Disponible', 9500, 'Zone très dense avec beaucoup de piétons.', 0),
+(12, 'Koumassi - Remblais, proche du marché', 4, 3, 'Big size', 'Disponible', 14000, 'Emplacement stratégique près d’un grand marché.', 0),
+(13, 'Bingerville - Entrée ville', 5, 3, 'Kilometrique', 'Disponible', 11000, 'Bonne visibilité en sortie d’autoroute.', 0),
+(14, 'Port-Bouët - Route aéroportuaire', 6, 4, 'City led', 'Réservé', 30000, 'Panneau numérique en face de l’aéroport.', 9),
+(15, 'Adjamé - Gare routière', 3.5, 2.5, 'Panneaux classiques', 'Disponible', 12500, 'Exposition maximale à la foule de la gare.', 0);
 
 -- --------------------------------------------------------
 
@@ -111,17 +128,14 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   `client_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `client_id` (`client_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `reservation`
 --
 
 INSERT INTO `reservation` (`id`, `date_deb`, `date_fin`, `statut`, `montant`, `client_id`) VALUES
-(4, '2025-05-14', '2025-05-31', 'Active', 45000, 1),
-(3, '2025-05-08', '2025-05-28', 'Active', 45000, 1),
-(5, '2025-05-15', '2025-05-29', 'Active', 45000, 1),
-(6, '2025-05-01', '2025-05-23', 'Active', 45000, 2);
+(9, '2025-05-17', '2025-06-17', 'Suspendue', 4464000, 1);
 
 -- --------------------------------------------------------
 
